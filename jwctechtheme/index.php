f08904197b19373445595944b7d49148e9c2e974
James Wyatt <jamesrwyatt35@gmail.com>
2026-01-02 21:03:45 -0500

    updates

--- jwctechtheme/index.php
+++ jwctechtheme/index.php
@@ -1,3 +1,14 @@
-<h1 class="text-4xl font-bold text-blue-600">
-    Tailwind in WordPress (manifest build) OK
-</h1>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<h1 class="text-4xl font-bold text-blue-600">Tailwind Test</h1>
+
+<?php wp_footer(); ?>
+</body>
+</html>
